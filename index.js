200e5f9dd4ecd4b3e57f3c137ff0c1b3c4975378
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-18 15:23:54 -0400

    add new logic

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-import express from "express";
+import express  from "express";
+import path from  "path"
+import { fileURLToPath } from "url";
+import{ requestLogger, apiHeaderSetter,ErrorHandler} from "./middleware.js";
+import apiRouter from  "./routes.js";
+import posts from "./data.js";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req,res) => {
-    res.send('hello world');
-})
+// templates
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// activating middlewares
+
+app.use(requestLogger);
+app.use(apiHeaderSetter);
+
+app.get("/", (req, res) => {
+    res.render("dashboard", { currentPosts: posts });
+});
+
+app.use("/api", apiRouter);
+
+// error middeelware
+app.use(ErrorHandler);
+
 
 app.listen(PORT, () => {
     console.log(`listening on port http://localhost:${PORT}`);
