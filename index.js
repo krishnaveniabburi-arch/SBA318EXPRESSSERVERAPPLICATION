5336a25feac97db0e80caae4c717feddfb5ab794
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-18 11:47:31 -0400

    add new file

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req,res) => {
+    res.send('hello world');
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port http://localhost:${PORT}`);
+})
