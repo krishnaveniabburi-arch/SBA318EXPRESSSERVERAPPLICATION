984d4d346b690c4233e34d3b3aa121c76cfb2c16
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-18 23:51:39 -0400

    add folder

---
+++ data/users.js
@@ -0,0 +1,33 @@
+ const users = [
+ { id: 1,
+      name: "Bob",
+      username: "Bob21",
+      email:"bob21example.com"
+    },
+    
+    {
+      id: 2,
+      name: "Bob",
+      username: "Cob22",
+      email:"cob21example.com"
+    },
+    {
+      id: 3,
+      name: "Bob",
+      username: "Dob21",
+      email:"dob21example.com"
+    },
+];
+
+export default users;
+
+
+const posts = [
+    { id: 1, userId: 1, title: "learning Express", content: "routers are straightforward.", category: "tech"},
+    { id: 2, userId: 2, title: "Css Tips", content: "Keep UI basic.", category: "design"},
+    { id: 2, userId:3, title: "Css Tips", content: "Keep UI basic.", category: "design"}
+    
+
+];
+
+export default posts;
