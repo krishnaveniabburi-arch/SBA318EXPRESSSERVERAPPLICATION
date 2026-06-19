804c4bc1eff25bd04021d6a0c70fbf2ce069518f
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-18 23:46:48 -0400

    remove folder

--- data/routes/comments.js
+++
@@ -1,9 +0,0 @@
-import users from "../data/users.js";
-import comments from "../routes/comments.js";
-import posts from "../data/posts.js";
-
-const comments = [{}];
-Router.route("/").get((req, res) => {
-    res.json(comments);
-});
-export default Router;
