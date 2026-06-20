9f9c067291753f480b70a91a9e17b741b405726e
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-20 15:47:11 -0400

    add new file

---
+++ data/comments.js
@@ -0,0 +1,16 @@
+const comments = [
+  {
+    id: 1,
+    postId: 1,
+    userId: 2,
+    text: "Great post!"
+  },
+  {
+    id: 2,
+    postId: 2,
+    userId: 1,
+    text: "Helpful tips."
+  }
+];
+
+export default comments;
